f802e8d5439599ffbcb128188d13f941c61deaa0
Ankit <ankitkumar777768@gmail.com>
2024-09-28 16:44:46 +0530

    add to github

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_first_element(arr):
+    return arr[2]
+
+def main():
+    numbers = [5,12,9,2,17,6]
+    result = get_first_element(numbers)
+    print("the first elem,ent is",result)
+
+if __name__ == "__main__":
+    main()
